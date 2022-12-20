062978783cc4c24b7b1aef7648f9409b3cf383d1
Balaji Senthilkumar <44097181+balaji-senthil@users.noreply.github.com>
2022-12-19 20:01:41 -0500

    added map

--- Chapter 5/practice/map.js
+++ Chapter 5/practice/map.js
@@ -1,1 +1,9 @@
-function map(array, fn)
+function map(array, fn){
+  let result = []
+  for(let element of array){
+    result.push(fn(element))
+  }
+  return result
+}
+
+console.log(map([1, 2, 3], e => e * e))
